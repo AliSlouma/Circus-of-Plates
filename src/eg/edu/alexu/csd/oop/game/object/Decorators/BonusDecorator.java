4e7ead706264778dc4343e3d2d13ebad75836172
slfyzz <48367677+slfyzz@users.noreply.github.com>
2019-12-19 14:53:45 +0200

    Decorator

--- src/eg/edu/alexu/csd/oop/game/object/Decorators/BonusDecorator.java
+++ src/eg/edu/alexu/csd/oop/game/object/Decorators/BonusDecorator.java
@@ -9,68 +9,73 @@ import java.awt.image.BufferedImage;
 public class BonusDecorator implements Shapes, GameObject {
 
     private Shapes shape;
-
     BonusDecorator(Shape gameObject)
     {
         shape = gameObject;
     }
+
     @Override
     public int getX() {
-        return 0;
+        return ((GameObject)shape).getX();
     }
 
     @Override
     public void setX(int x) {
-
+        ((GameObject)shape).setX(x);
     }
 
     @Override
     public int getY() {
-        return 0;
+        return ((GameObject)shape).getY();
     }
 
     @Override
     public void setY(int y) {
-
+        ((GameObject)shape).setY(y);
     }
 
     @Override
     public int getWidth() {
-        return 0;
+        return ((GameObject)shape).getWidth();
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        return ((GameObject)shape).getHeight();
     }
 
     @Override
     public boolean isVisible() {
-        return false;
+        return ((GameObject)shape).isVisible();
     }
 
     @Override
     public BufferedImage[] getSpriteImages() {
-        return new BufferedImage[0];
+        return new BufferedImage[0].;
     }
 
     @Override
     public boolean isused() {
-        return false;
+        return shape.isused();
     }
 
     @Override
     public void use(boolean used) {
-
+        shape.use(used);
     }
 
     @Override
     public boolean isSimilar(GameObject shape1, GameObject shape2, GameObject shape3) {
-        return false;
+        return shape.isSimilar(shape1, shape2, shape3);
     }
 
     @Override
     public int getScore() {
-        return 0;
+        return 50 + shape.getScore();
+    }
+
+    @Override
+    public GameObject getShape() {
+        return (GameObject) shape;
     }
 }
